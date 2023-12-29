569f11794130b7d11c94e5692ffb4004c989241c
dierkrybinski <33806771+dierkrybinski@users.noreply.github.com>
2023-12-29 21:58:29 +0100

    Update module.php

--- Form1/module.php
+++ Form1/module.php
@@ -89,7 +89,7 @@ class FormularTest extends ipsmodule
         }
         if (!$this->isConnected) {
             $this->SendDebug('Connect [' . $_IPS['THREAD'] . ']', 'Start ' . sprintf('%.3f', ((microtime(true) - $this->Runtime) * 1000)) . ' ms', 0);
-			$connectionInfo = array( "Database"=>"IPSLOG, "UID"=>"HA\Administrator", "PWD"=>"JeTgDr#1981");
+			$connectionInfo = array( "Database"=>"IPSLOG", "UID"=>"HA\Administrator", "PWD"=>"JeTgDr#1981");
             $this->DB = $conn = sqlsrv_connect( "HASQL01\IPS", $connectionInfo);;
             if ($this->DB->connect_errno == 0) {
                 $this->isConnected = true;
