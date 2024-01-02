67dba492b6d791852f09d5ecf1f26abb933e8707
dierkrybinski <33806771+dierkrybinski@users.noreply.github.com>
2024-01-02 18:17:08 +0100

    Update module.php

--- Form1/module.php
+++ Form1/module.php
@@ -18,15 +18,11 @@ class FormularTest extends ipsmodule
         $this->RegisterPropertyString('Username', '');
         $this->RegisterPropertyString('Password', '');
         $this->RegisterPropertyString('Database', 'IPSLOG');
-        $this->RegisterPropertyString('Variables', json_encode([]));
-        $this->RegisterTimer('LogData', 0, 'ACmySQL_LogData($_IPS[\'TARGET\']);');
-        $this->Vars = [];
-        $this->Buffer = [];
 		
-		        if (!$this->Login()) {
-            echo $this->Translate('Cannot connect to database.');
-            $this->SetStatus(IS_EBASE + 2);
-            return;
+        if (!$this->Login()) {
+        echo $this->Translate('Cannot connect to database.');
+        $this->SetStatus(IS_EBASE + 2);
+        return;
         }
     }
 
@@ -51,36 +47,7 @@ class FormularTest extends ipsmodule
         return json_encode($form);
     }
     
-        /**
-     * Interne Funktion des SDK.
-     */
-    public function FTest($NR)
-    {
-        $myfile = fopen("c:\\programdata\\symcon\\webfront\\user\\newfile.txt", "w") or die("Unable to open file!");
-        $txt = "<!DOCTYPE html>\n";
-        fwrite($myfile, $txt);
-        $txt = "<html lang=\"de\">\n";
-        fwrite($myfile, $txt);
-        $txt = "<body>\n";
-        fwrite($myfile, $txt);
-        $txt = "<table>\n";
-        fwrite($myfile, $txt);
-        $txt = "<tr>\n";
-        fwrite($myfile, $txt);
-        $txt = "<td>02</td><td>0a</td>\n";
-        fwrite($myfile, $txt);
-        $txt = "</tr>\n";
-        fwrite($myfile, $txt);
-        $txt = "</tabele>\n";
-        fwrite($myfile, $txt);
-        $txt = "</body>\n";
-        fwrite($myfile, $txt);
-        fclose($myfile);
-        $Result = $NR + $NR;
-        return json_encode($Result);
-    }
-	
-	private $isConnected = false;
+    private $isConnected = false;
 
     protected function Login()
     {
@@ -89,14 +56,7 @@ class FormularTest extends ipsmodule
         }
         if (!$this->isConnected) {
             $this->SendDebug('Connect [' . $_IPS['THREAD'] . ']', 'Start ' . sprintf('%.3f', ((microtime(true) - $this->Runtime) * 1000)) . ' ms', 0);
-			$connectionInfo = array( "Database"=>"IPSLOG", "UID"=>"HA\\Administrator", "PWD"=>"JeTgDr#1981");
-            $this->DB = $conn = sqlsrv_connect( "HASQL01\\IPS", $connectionInfo);
-			print_r( sqlsrv_errors(), true);
-			echo $this->Translate('$conn: ' . $conn);
-			if ($conn)
-			{ 
-				return true;
-			}
+            $this->DB = @new \mysqli('p:' . $this->ReadPropertyString('Host'), $this->ReadPropertyString('Username'), $this->ReadPropertyString('Password'));
             if ($this->DB->connect_errno == 0) {
                 $this->isConnected = true;
                 $this->SendDebug('Login [' . $_IPS['THREAD'] . ']', sprintf('%.3f', ((microtime(true) - $this->Runtime) * 1000)) . ' ms', 0);
@@ -106,8 +66,6 @@ class FormularTest extends ipsmodule
         }
         return true;
     }
-    
-
 }
 
 /* @} */
